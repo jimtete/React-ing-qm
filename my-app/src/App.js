0de0719f4079bd735c89abc24a45e889c5b2e66f
jimtete <tetepoulidis@ieee.org>
2022-11-24 10:27:57 +0200

    adding new expense component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,6 @@
 import TotalExpenses from "./components/Expenses/TotalExpenses";
 import React from "react";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 function App() {
   const expenses = [
@@ -32,7 +33,7 @@ function App() {
   // );
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense/>
       <TotalExpenses items={expenses}/>
     </div>
   );
--- my-app/src/components/Expenses/ExpenseItem.js
+++ my-app/src/components/Expenses/ExpenseItem.js
@@ -1,15 +1,23 @@
 import ExpenseDate from "./ExpenseDate";
+import React, { useState } from "react";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 function ExpenseItem(args) {
+  const [title, setTitle] = useState(args.title);
+
+  function clickHandler() {
+    setTitle("Updated!");
+  }
+
   return (
     <Card className="expense-item">
-      <ExpenseDate date = {args.date}/>
+      <ExpenseDate date={args.date} />
       <div className="expense-item__description">
-        <h2>{args.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">{args.amount}$</div>
       </div>
+      <button onClick={clickHandler}>CHange title</button>
     </Card>
   );
 }
