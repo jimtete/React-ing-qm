e4b3461ec165f843303278b6b300ce6a5e1ae326
jimtete <tetepoulidis@ieee.org>
2022-11-24 15:52:03 +0200

    adding ExpenseFilter implementation

--- my-app/src/components/Expenses/TotalExpenses.js
+++ my-app/src/components/Expenses/TotalExpenses.js
@@ -1,31 +1,44 @@
+import React, { useState } from 'react';
+
 import ExpenseItem from "./ExpenseItem";
+import ExpensesFilter from "./ExpensesFilter";
 import Card from "../UI/Card";
-import './TotalExpenses.css'
+import "./TotalExpenses.css";
 
 function TotalExpenses(args) {
+  const [filteredYear, setFilteredYear] = useState('2019');
+  
+  function filterChangeHandler(selectedYear){
+    setFilteredYear(selectedYear);
+  }
+
+
   return (
-    <Card className="expenses">
-      <ExpenseItem
-        title={args.items[0].title}
-        amount={args.items[0].amount}
-        date={args.items[0].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={args.items[1].title}
-        amount={args.items[1].amount}
-        date={args.items[1].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={args.items[2].title}
-        amount={args.items[2].amount}
-        date={args.items[2].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={args.items[3].title}
-        amount={args.items[3].amount}
-        date={args.items[3].date}
-      ></ExpenseItem>
-    </Card>
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter selected={filteredYear} onYearSelectedFilter={filterChangeHandler} />
+        <ExpenseItem
+          title={args.items[0].title}
+          amount={args.items[0].amount}
+          date={args.items[0].date}
+        ></ExpenseItem>
+        <ExpenseItem
+          title={args.items[1].title}
+          amount={args.items[1].amount}
+          date={args.items[1].date}
+        ></ExpenseItem>
+        <ExpenseItem
+          title={args.items[2].title}
+          amount={args.items[2].amount}
+          date={args.items[2].date}
+        ></ExpenseItem>
+        <ExpenseItem
+          title={args.items[3].title}
+          amount={args.items[3].amount}
+          date={args.items[3].date}
+        ></ExpenseItem>
+      </Card>
+    </div>
   );
 }
 
