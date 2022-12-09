7fa517fe472b75b60ca97beff2920b5e6fce3947
jimtete <tetepoulidis@ieee.org>
2022-12-09 14:59:23 +0200

    implementing component logic for user finder

---
+++ 04-starting-project/src/components/UserFinder.js
@@ -0,0 +1,50 @@
+import { Fragment, useState, useEffect, Component } from "react";
+import classes from "./UserFinder.module.css";
+import Users from "./Users";
+
+const DUMMY_USERS = [
+  { id: "u1", name: "Max" },
+  { id: "u2", name: "Manuel" },
+  { id: "u3", name: "Julie" },
+];
+
+class UserFinder extends Component {
+  constructor() {
+    super();
+    this.state = {
+      filteredUsers: [],
+      searchTerm: " ",
+    };
+  }
+
+  componentDidMount(){
+    this.setState({filteredUsers: DUMMY_USERS});
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.searchTerm !== this.state.searchTerm) {
+      this.setState({
+        filteredUsers: DUMMY_USERS.filter((user) => {
+          return user.name.includes(this.state.searchTerm);
+        }),
+      });
+    }
+  }
+
+  searchChangeHandler(event) {
+    this.setState({ searchTerm: event.target.value });
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <div className={classes.finder}>
+          <input type="search" onChange={this.searchChangeHandler.bind(this)} />
+        </div>
+        <Users users={this.state.filteredUsers} />
+      </Fragment>
+    );
+  }
+}
+
+export default UserFinder;
