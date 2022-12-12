a492c3497b23604dd717279349de19e5bdd0b301
jimtete <tetepoulidis@ieee.org>
2022-12-12 15:59:05 +0200

    adding more value to the custom hook

--- 06-starting-project/src/hooks/use-counter.js
+++ 06-starting-project/src/hooks/use-counter.js
@@ -1,15 +1,19 @@
-import { useState, useEffect} from 'react';
+import { useState, useEffect } from "react";
 
-export default function useCounter() {
+export default function useCounter(forwards = true) {
   const [counter, setCounter] = useState(0);
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setCounter((prevCounter) => prevCounter + 1);
+      if (forwards) {
+        setCounter((prevCounter) => prevCounter + 1);
+      } else {
+        setCounter((prevCounter) => prevCounter - 1);
+      }
     }, 1000);
 
     return () => clearInterval(interval);
-  }, []);
+  }, [forwards]);
 
   return counter;
 }
