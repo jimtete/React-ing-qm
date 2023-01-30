6432fa56ff85baf44e21c238140056a79a6cb025
jimtete <tetepoulidis@ieee.org>
2023-01-30 15:45:31 +0200

    added error page - handling?

--- 14-starting-project/src/App.js
+++ 14-starting-project/src/App.js
@@ -1,26 +1,22 @@
-import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  RouterProvider,
-  Route,
-} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import HomePage from "./pages/Home";
 import ProductsPage from "./pages/Products";
+import RootLayout from "./pages/Root";
+import ErrorPage from "./pages/Error";
 
-const routeDefinitions = createRoutesFromElements(
-  <Route>
-    <Route path="/" element={<HomePage></HomePage>}/>
-    <Route path="/products" element={<ProductsPage></ProductsPage>}/>
-  </Route>
-);
-
-const router = createBrowserRouter(routeDefinitions);
-
-// const router = createBrowserRouter([
-//   { path: "/", element: <HomePage /> },
-//   { path: "/products", element: <ProductsPage /> },
-// ]);
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      { path: "/", element: <HomePage /> },
+      { path: "/products", element: <ProductsPage /> },
+    ],
+  },
+  ,
+]);
 
 function App() {
   return <RouterProvider router={router} />;
