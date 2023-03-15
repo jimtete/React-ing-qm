1dd887d94afb47e1fe842c372ecae2894838bd8b
jimtete <tetepoulidis@ieee.org>
2023-03-15 10:35:28 +0200

    adding more components

---
+++ 22-starting-project/react-ts/src/components/NewTodo.tsx
@@ -0,0 +1,12 @@
+const NewTodo = () => {
+  const submitHandler = (event: React.FormEvent) => {
+    event.preventDefault();
+  };
+  return (
+    <form onSubmit={submitHandler}>
+      <label htmlFor="text">Todo text</label>
+      <input type="text" id="text" />
+      <button>Add Todo</button>
+    </form>
+  );
+};
