4e5ec184a1ad6c63c66a06c5b229ece44bf82f84
jimtete <tetepoulidis@ieee.org>
2023-03-23 13:35:06 +0200

    changing from useState to useReducer and handling errors and loading.

--- 23-starting-project/src/components/Ingredients/Ingredients.js
+++ 23-starting-project/src/components/Ingredients/Ingredients.js
@@ -1,11 +1,28 @@
-import React, { useState, useEffect, useCallback } from "react";
+import React, { useState, useReducer, useEffect, useCallback } from "react";
 
+import ErrorModal from "../UI/ErrorModal";
 import IngredientForm from "./IngredientForm";
 import IngredientList from "./IngredientList";
 import Search from "./Search";
 
+const ingredientReducer = (state, action) => {
+  switch (action.type) {
+    case "SET":
+      return action.ingredients;
+    case "ADD":
+      return [...state, action.ingredient];
+    case "DELETE":
+      return state.filter((i) => i.id != action.id);
+    default:
+      throw new Error("Should not get there!");
+  }
+};
+
 const Ingredients = () => {
-  const [ingredients, setIngredients] = useState([]);
+  const [ingredients, dispatch] = useReducer(ingredientReducer, []);
+  //const [ingredients, setIngredients] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
 
   useEffect(() => {
     fetch(
@@ -25,15 +42,16 @@ const Ingredients = () => {
             amount: responseData[key].amount,
           });
         }
-        setIngredients(loadedIngredients);
+        dispatch({ type: "SET", ingredients: loadedIngredients });
       });
   }, []);
 
   const filteredIngredientsHandler = useCallback((filteredIngredients) => {
-    setIngredients(filteredIngredients);
+    dispatch({ type: "SET", ingredients: filteredIngredients });
   }, []);
 
   const addIngredientHandler = (ingredient) => {
+    setIsLoading(true);
     fetch(
       "https://react-http-c318b-default-rtdb.europe-west1.firebasedatabase.app/ingredients.json",
       {
@@ -43,10 +61,15 @@ const Ingredients = () => {
       }
     )
       .then((response) => {
+        setIsLoading(false);
         return response.json();
       })
       .then((responseData) => {
-        setIngredients((prevState) => {
+        dispatch({
+          type: "ADD",
+          ingredient: { id: responseData.name, ...ingredient },
+        });
+        /*setIngredients((prevState) => {
           return [
             ...prevState,
             {
@@ -54,19 +77,45 @@ const Ingredients = () => {
               ...ingredient,
             },
           ];
-        });
+        });*/
+      })
+      .catch((error) => {
+        setError(error.message);
       });
   };
 
   const removeIngredientHandler = (ingredientId) => {
-    setIngredients((prevState) => {
-      return prevState.filter((i) => i.id !== ingredientId);
-    });
+    setIsLoading(true);
+    fetch(
+      `https://react-http-c318b-default-rtdb.europe-west1.firebasedatabase.app/ingredients/${ingredientId}.json`,
+      {
+        method: "DELETE",
+      }
+    )
+      .then((response) => {
+        setIsLoading(false);
+        /*setIngredients((prevState) => {
+          return prevState.filter((i) => i.id !== ingredientId);
+        });*/
+        dispatch({ type: "DELETE", id: ingredientId });
+      })
+      .catch((error) => {
+        setError(error.message);
+      });
+  };
+
+  const clearError = () => {
+    setError(null);
+    setIsLoading(false);
   };
 
   return (
     <div className="App">
-      <IngredientForm onAddIngredient={addIngredientHandler} />
+      {error && <ErrorModal onClose={clearError}>{error}</ErrorModal>}
+      <IngredientForm
+        onAddIngredient={addIngredientHandler}
+        loading={isLoading}
+      />
 
       <section>
         <Search onLoadIngredients={filteredIngredientsHandler} />
--- 23-starting-project/src/components/Ingredients/Search.js
+++ 23-starting-project/src/components/Ingredients/Search.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 
 import Card from "../UI/Card";
 import "./Search.css";
@@ -6,39 +6,49 @@ import "./Search.css";
 const Search = React.memo((props) => {
   const { onLoadIngredients } = props;
   const [enteredFilter, setEnteredFilter] = useState("");
+  const inputRef = useRef();
 
   useEffect(() => {
-    const query =
-      enteredFilter.length === 0
-        ? ""
-        : `?orderBy="title"&equalTo="${enteredFilter}"`;
-    fetch(
-      "https://react-http-c318b-default-rtdb.europe-west1.firebasedatabase.app/ingredients.json" +
-        query,
-      {
-        method: "GET",
-        headers: { "Content-Type": "application/json" },
-      }
-    )
-      .then((response) => response.json())
-      .then((responseData) => {
-        const loadedIngredients = [];
-        for (const key in responseData) {
-          loadedIngredients.push({
-            id: key,
-            title: responseData[key].title,
-            amount: responseData[key].amount,
+    const timer = setTimeout(() => {
+      if (enteredFilter === inputRef.current.value) {
+        const query =
+          enteredFilter.length === 0
+            ? ""
+            : `?orderBy="title"&equalTo="${enteredFilter}"`;
+        fetch(
+          "https://react-http-c318b-default-rtdb.europe-west1.firebasedatabase.app/ingredients.json" +
+            query,
+          {
+            method: "GET",
+            headers: { "Content-Type": "application/json" },
+          }
+        )
+          .then((response) => response.json())
+          .then((responseData) => {
+            const loadedIngredients = [];
+            for (const key in responseData) {
+              loadedIngredients.push({
+                id: key,
+                title: responseData[key].title,
+                amount: responseData[key].amount,
+              });
+            }
+            onLoadIngredients(loadedIngredients);
           });
-        }
-        onLoadIngredients(loadedIngredients);
-      });
-  }, [enteredFilter, onLoadIngredients]);
+      }
+    }, 500);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [enteredFilter, onLoadIngredients, inputRef]);
   return (
     <section className="search">
       <Card>
         <div className="search-input">
           <label>Filter by Title</label>
           <input
+            ref={inputRef}
             type="text"
             value={enteredFilter}
             onChange={(event) => setEnteredFilter(event.target.value)}
